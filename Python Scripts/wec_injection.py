ebc44223800edded00593f73142a6c3a6f08ba94
Alex Barajas <barajale@oregonstate.edu>
2023-01-20 17:12:23 -0800

    SQL working and sim run now part of class

--- Python Scripts/wec_injection.py
+++ Python Scripts/wec_injection.py
@@ -1,21 +1,26 @@
-# AC power flow
+# DC/AC power flow
 
 import sys
-sys.path.append('../wec-grid-code') #
+sys.path.append('../wec-grid-code') # 
 import wec_grid_class as wg
-import pandas as pd
 import sqlite3
+import pandas as pd
 
 
 def main():
   # Read in case file
   case14 = r"../input_files/case14.raw"
+  solver = input("Solver: ")
 
   # initalize WEC GRID Object
-  pf = wg.Wec_grid(case14,"fnsl",3)
+  pf = wg.Wec_grid(case14,solver,3)
 
   # Run first Power Flow and get bus values
   print(pf.dataframe)
+
+  sim = input("Run WEC Simulator?")
+  if sim == "yes":
+    pf.run_WEC_Sim()
 
   # Read in Wec set point values
   con = sqlite3.connect("../input_files/r2g_database.db")
@@ -25,8 +30,11 @@ def main():
   for i in range(len(injection)):
     print("Time: {}".format(injection.iloc[i].time))
     print("P setpoint: {}".format(injection.pg.iloc[i]))
-
-    pf.ac_injection(pf.wecBus_num, injection.pg.iloc[i], injection.vs.iloc[i],pf.solver, injection.iloc[i].time)
+    if pf.solver == 'AC':
+        pf.ac_injection(pf.wecBus_num, injection.pg.iloc[i], injection.vs.iloc[i],
+                        pf.solver, injection.iloc[i].time)
+    if pf.solver == 'DC':
+        pf.dc_injection(pf.wecBus_num, injection.pg.iloc[i], pf.solver, injection.iloc[i].time)
     print("========================")
-    exit()
+  exit()
 main()

2736987f583a277461e3bc0780bc0ebaf80e94ef
Alex Barajas <barajale@oregonstate.edu>
2023-06-22 14:00:16 -0700

    pip install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wec_grid',
+    version='0.1',
+    packages=find_packages(),
+    description='A description of your project.',
+    install_requires=[
+        # list the packages your project depends on
+    ],
+)
